118fa7b90adb64e79132c93b3338958d8c299eb3
Alan Garduño <alango39@gmail.com>
2020-01-07 19:15:55 -0600

    [ADD] TAD

---
+++ tad_1.c
@@ -0,0 +1,31 @@
+/*
+Ejemplo de recursividad en lenguage C
+
+Para compilar: gcc recursividad_1.c -o testA.out // gcc recursividad_1.c -o testA.exe
+Para ejecutar: ./testA.out(Linux/Mac)
+                testA.exe (Windows)
+
+Autor: Alan Garduño Velazquez
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+// Declaracion de un estructura y su alias
+typedef struct Fraccion {
+  // Atributos o propiedades
+  int denominador;
+  int numerador;
+}Fraccion;
+
+int main(){
+
+  Fraccion f; //Varaible normal
+  Fraccion * fptr; // Puntero
+  f.denominador = 10; //Acc
+  f.numerador = 20; // Acceso a las propiedades por operador .
+  fptr->denominador=10;
+  fptr->numerador=80; // Acceso a las propiedades por operador flecha
+
+  return 0;
+  
+}
